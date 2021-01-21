e173f683c07fccd95fe27d9fb97b5ffbddf9db8e
Daniel Costelloe <danny.costelloe@gmail.com>
2021-01-21 16:23:26 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -8,3 +8,20 @@ WIN_COMBINATIONS = [
   [0,4,8],  # Forward diagonal
   [2,4,6]  # Backward diagonal
 ]
+
+#helper methods
+
+#display_board creates a blank tic tac toe board
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+  puts "-----------"
+end
+
+#input_to_index converts a user_input to an integer
+def input_to_index(input)
+  index = input.to_i - 1
+end
